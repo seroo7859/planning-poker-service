ad198ea6d13cd8996d7633b1e4084cc3ec508460
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-04-30 18:47:01 +0200

    initial commit

---
+++ src/main/java/de/bht/planningpoker/PlanningPokerServiceApplication.java
@@ -0,0 +1,13 @@
+package de.bht.planningpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bht/planningpoker/PlanningPokerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bht.planningpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
