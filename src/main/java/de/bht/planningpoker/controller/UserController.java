6aff675f613f7e514c41fb70fe9d84ed6d0b7e04
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 19:07:01 +0200

    add user controller

---
+++ src/main/java/de/bht/planningpoker/controller/UserController.java
@@ -0,0 +1,25 @@
+package de.bht.planningpoker.controller;
+
+import de.bht.planningpoker.controller.dto.UserDto;
+import de.bht.planningpoker.controller.mapper.UserMapper;
+import de.bht.planningpoker.model.User;
+import de.bht.planningpoker.service.UserService;
+import de.bht.planningpoker.service.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController implements UserResource {
+
+    private final UserService service;
+    private final UserMapper mapper;
+
+    @Override
+    public ResponseEntity<UserDto> getCurrent() throws ServiceException {
+        User currentUser = service.getCurrentUser();
+        return ResponseEntity.ok(mapper.mapToDto(currentUser));
+    }
+
+}
---
+++ src/main/java/de/bht/planningpoker/controller/UserResource.java
@@ -0,0 +1,34 @@
+package de.bht.planningpoker.controller;
+
+import de.bht.planningpoker.controller.dto.UserDto;
+import de.bht.planningpoker.service.exception.ServiceException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Tag(name = "User", description = "All about users")
+@RequestMapping(value = UserResource.BASE_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public interface UserResource {
+
+    String BASE_URL = "/api/user";
+
+    @GetMapping(value = "/info")
+    @ResponseBody
+    @Operation(summary = "Returns the current user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found", content = @Content(schema = @Schema(implementation = UserDto.class))),
+            @ApiResponse(responseCode = "401", description = "User not authenticated", content = @Content)
+    })
+    @SecurityRequirement(name = "bearer-auth")
+    ResponseEntity<UserDto> getCurrent() throws ServiceException;
+
+}
