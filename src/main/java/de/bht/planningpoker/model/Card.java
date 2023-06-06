1d97826ced7f1504ce06b4963499ad4f1dace0fe
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 17:59:40 +0200

    add card deck model

---
+++ src/main/java/de/bht/planningpoker/model/Card.java
@@ -0,0 +1,74 @@
+package de.bht.planningpoker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Label may not be blank")
+    @Size(min = 1, max = 32, message = "Value must be between 1 and 32 characters long")
+    @Column(nullable = false, length = 32)
+    private String label;
+
+    @NotBlank(message = "Value may not be blank")
+    @Size(min = 1, max = 3, message = "Value must be between 1 and 3 characters long")
+    @Column(nullable = false, length = 3)
+    private String value;
+
+    @NotBlank(message = "FrontTextColor may not be blank")
+    @Size(min = 3, max = 6, message = "FrontTextColor must be 3 or 6 characters long")
+    @Pattern(regexp = "^([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$", message = "FrontTextColor must be hexadecimal color value without hashtag")
+    @Column(nullable = false, length = 6)
+    private String frontTextColor;
+
+    @NotBlank(message = "BackTextColor may not be blank")
+    @Size(min = 3, max = 6, message = "BackTextColor must be 3 or 6 characters long")
+    @Pattern(regexp = "^([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$", message = "BackTextColor must be hexadecimal color value without hashtag")
+    @Column(nullable = false, length = 6)
+    private String backTextColor;
+
+    @NotBlank(message = "BackgroundColor may not be blank")
+    @Size(min = 3, max = 6, message = "BackgroundColor must be 3 or 6 characters long")
+    @Pattern(regexp = "^([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$", message = "BackgroundColor must be hexadecimal color value without hashtag")
+    @Column(nullable = false, length = 6)
+    private String backgroundColor;
+
+    @NotBlank(message = "HoverColor may not be blank")
+    @Size(min = 3, max = 6, message = "HoverColor must be 3 or 6 characters long")
+    @Pattern(regexp = "^([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$", message = "HoverColor must be hexadecimal color value without hashtag")
+    @Column(nullable = false, length = 6)
+    private String hoverColor;
+
+    @NotNull(message = "CreatedAt may not be null")
+    @PastOrPresent(message = "CreatedAt must be in the past or in the present")
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @NotNull(message = "UpdatedAt may not be null")
+    @PastOrPresent(message = "UpdatedAt must be in the past or in the present")
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected Card() {}
+
+}
---
+++ src/main/java/de/bht/planningpoker/model/Deck.java
@@ -0,0 +1,64 @@
+package de.bht.planningpoker.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Deck {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name may not be blank")
+    @Size(min = 1, max = 32, message = "Name must be between 1 and 32 characters long")
+    @Pattern(regexp = "^[A-Za-z0-9 _\\-]{1,32}$", message = "Name must be alphanumeric characters and can have white spaces, underscores and hyphens")
+    @Column(nullable = false, length = 32)
+    private String name;
+
+    @UniqueElements(message = "Cards must not contain duplicates")
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(
+        name = "deck_card",
+        joinColumns = @JoinColumn(name = "deck_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "card_id", referencedColumnName = "id", unique = true))
+    @OrderColumn(name="list_index")
+    private List<@Valid Card> cards = new ArrayList<>();
+
+    @NotNull(message = "CreatedAt may not be null")
+    @PastOrPresent(message = "CreatedAt must be in the past or in the present")
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @NotNull(message = "UpdatedAt may not be null")
+    @PastOrPresent(message = "UpdatedAt must be in the past or in the present")
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected Deck() {}
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
+}
