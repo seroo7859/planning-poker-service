405f24c8c55b7cc2555b3ece709551b6e7e59264
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 20:55:27 +0200

    refactor backlog repository

--- src/main/java/de/bht/planningpoker/repository/BacklogRepository.java
+++ src/main/java/de/bht/planningpoker/repository/BacklogRepository.java
@@ -1,12 +1,8 @@
 package de.bht.planningpoker.repository;
 
 import de.bht.planningpoker.model.Backlog;
-import de.bht.planningpoker.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
