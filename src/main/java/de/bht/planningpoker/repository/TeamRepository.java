f0d49f3ea503336b36e51a4d2e87a3839125befe
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 20:32:15 +0200

    add query methods to team repository

--- src/main/java/de/bht/planningpoker/repository/TeamRepository.java
+++ src/main/java/de/bht/planningpoker/repository/TeamRepository.java
@@ -1,9 +1,21 @@
 package de.bht.planningpoker.repository;
 
 import de.bht.planningpoker.model.Team;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    boolean existsBySessionPublicId(String sessionId);
+
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = { "members" }
+    )
+    Optional<Team> findBySessionPublicId(String sessionId);
+
 }
