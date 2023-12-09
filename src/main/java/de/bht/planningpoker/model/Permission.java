6c53e74ea3d1449f998304d01bf1d1b7596c2cfb
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-12-09 20:09:48 +0100

    change permission model

--- src/main/java/de/bht/planningpoker/model/Permission.java
+++ src/main/java/de/bht/planningpoker/model/Permission.java
@@ -7,11 +7,26 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Permission {
-    SESSION_READ("session:read"),
     SESSION_CREATE("session:create"),
     SESSION_DELETE("session:delete"),
     SESSION_JOIN("session:join"),
-    SESSION_LEAVE("session:leave");
+    SESSION_LEAVE("session:leave"),
+    BACKLOG_IMPORT("backlog:import"),
+    BACKLOG_EXPORT("backlog:export"),
+    BACKLOG_CLEAR("backlog:clear"),
+    BACKLOG_RENAME("backlog:rename"),
+    BACKLOG_READ("backlog:read"),
+    BACKLOG_ITEM_ADD("backlog-item:add"),
+    BACKLOG_ITEM_REMOVE("backlog-item:remove"),
+    BACKLOG_ITEM_UPDATE("backlog-item:update"),
+    ESTIMATION_ROUND_START("estimation-round:start"),
+    ESTIMATION_ROUND_FINISH("estimation-round:finish"),
+    ESTIMATION_GIVE("estimation:give"),
+    ESTIMATION_SUMMARY_READ("estimation-summary:read"),
+    DISCUSSION_START("discussion:start"),
+    DISCUSSION_STOP("discussion:stop"),
+    DISCUSSION_POST_CREATE("discussion-post:create"),
+    DISCUSSION_POST_READ("discussion-post:read");
 
     @NotBlank(message = "Permission may not be blank")
     @Pattern(regexp = "^[a-z]+:[a-z]+$", message = "Permission must be lower case letter from a to z and match with format <entity>:<operation>")
