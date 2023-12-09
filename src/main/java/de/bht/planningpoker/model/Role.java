5bb1e2b26128d569c4072f82aefca36f9c10162a
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-12-09 20:10:17 +0100

    change role model

--- src/main/java/de/bht/planningpoker/model/Role.java
+++ src/main/java/de/bht/planningpoker/model/Role.java
@@ -14,9 +14,52 @@ import static de.bht.planningpoker.model.Permission.*;
 
 @RequiredArgsConstructor
 public enum Role {
-    MODERATOR(Set.of(SESSION_CREATE, SESSION_READ, SESSION_DELETE, SESSION_JOIN, SESSION_LEAVE)),
-    PARTICIPANT(Set.of(SESSION_READ, SESSION_JOIN, SESSION_LEAVE)),
-    SPECTATOR(Set.of(SESSION_READ, SESSION_JOIN, SESSION_LEAVE));
+    ANONYMOUS(
+        Set.of(
+            SESSION_CREATE,
+            SESSION_JOIN
+        )
+    ),
+    MODERATOR(
+        Set.of(
+            SESSION_LEAVE,
+            SESSION_DELETE,
+            BACKLOG_IMPORT,
+            BACKLOG_EXPORT,
+            BACKLOG_CLEAR,
+            BACKLOG_RENAME,
+            BACKLOG_READ,
+            BACKLOG_ITEM_ADD,
+            BACKLOG_ITEM_REMOVE,
+            BACKLOG_ITEM_UPDATE,
+            ESTIMATION_ROUND_START,
+            ESTIMATION_ROUND_FINISH,
+            ESTIMATION_GIVE,
+            ESTIMATION_SUMMARY_READ,
+            DISCUSSION_START,
+            DISCUSSION_STOP,
+            DISCUSSION_POST_CREATE,
+            DISCUSSION_POST_READ
+        )
+    ),
+    PARTICIPANT(
+        Set.of(
+            SESSION_LEAVE,
+            BACKLOG_READ,
+            ESTIMATION_GIVE,
+            ESTIMATION_SUMMARY_READ,
+            DISCUSSION_POST_CREATE,
+            DISCUSSION_POST_READ
+        )
+    ),
+    SPECTATOR(
+        Set.of(
+            SESSION_LEAVE,
+            BACKLOG_READ,
+            ESTIMATION_SUMMARY_READ,
+            DISCUSSION_POST_READ
+        )
+    );
 
     @NotNull(message = "permissions may not be null")
     @Getter
